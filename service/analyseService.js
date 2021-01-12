ac2ee15066e0f31a0a212987e839ac109735c974
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-01-12 09:07:07 +0600

    edited comments

--- service/analyseService.js
+++ service/analyseService.js
@@ -7,11 +7,11 @@ module.exports={
             },
             formattedEdgeData=[] // This will contain formatted edge data that is required for csv output.
         entries.map(entry=>{
-            var key=entry.date+'#'+entry.shift // Simultaneous shifts (same date and time) will be stored against the same field
-            if(!(key in data))data[key]=[] // Initializing array that'd contain the names of persons working at the same time
+            var key=entry.date+'#'+entry.shift // Simultaneous shifts (same date and time) will be stored against the same key
+            if(!(key in data))data[key]=[] // Initializing array that'd contain the names of persons working at the same shift
             data[key].push(entry.volunteerName)
             if(!graph.nodes.includes(entry.volunteerName))graph.nodes.push(entry.volunteerName) // Adding node to graph if it is not added yet
-            if(data[key].length>1) // If other persons are working at the same date and shift
+            if(data[key].length>1) // If other persons are working at the same day and shift
                 for(var i=0;i<data[key].length-1;i++){
                     var edgeIndices=[graph.nodes.indexOf(data[key][i]),graph.nodes.indexOf(entry.volunteerName)].sort() //sorting the 2 vertices that the edge connects as it is bidirectional
                     var edgeKey=edgeIndices[0]+'#'+edgeIndices[1] //Naming the edge
